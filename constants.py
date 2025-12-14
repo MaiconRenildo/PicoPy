049c97b945ec44f06d907bba30eb9dfe5c7ec9d6
Maicon Renildo <maicon.renildo1@gmail.com>
2025-12-14 11:23:41 -0300

    feat: rascunho do init

---
+++ constants.py
@@ -0,0 +1,27 @@
+# Título padrão da janela
+PICO_TITLE = "pico-SDL"
+
+# Dimensões padrão
+PICO_DIM_WINDOW = (640, 360)
+PICO_DIM_WORLD = (64, 36)
+
+# Tamanho da tabela hash
+PICO_HASH = 128
+
+# Valores de anchor (horizontal)
+PICO_LEFT = 0
+PICO_CENTER = 50
+PICO_RIGHT = 100
+
+# Valores de anchor (vertical)
+PICO_TOP = 0
+PICO_MIDDLE = 50
+PICO_BOTTOM = 100
+
+# Estilos de desenho
+PICO_FILL = 0
+PICO_STROKE = 1
+
+# Clip reset (usado para desabilitar clipping)
+PICO_CLIP_RESET = (0, 0, 0, 0)
+
