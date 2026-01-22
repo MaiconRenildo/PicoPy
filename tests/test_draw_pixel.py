b5bd90110483650a123b7983e735a2354ccef4c8
Maicon Renildo <63758491+MaiconRenildo@users.noreply.github.com>
2026-01-21 22:54:21 -0300

    feat: getters and setters

--- tests/test_draw_pixel.py
+++ tests/test_draw_pixel.py
@@ -1,4 +1,4 @@
-from pico import pico_init, pico_output_clear, pico_output_draw_pixel, pico_output_draw_pixels
+from pico import pico_init, pico_output_clear, pico_output_draw_pixel, pico_output_draw_pixels, pico_get_dim_window, pico_set_dim_window, pico_get_dim_world, pico_set_dim_world, pico_set_zoom, pico_output_present
 from .utils import screenshot_and_compare
 
 
@@ -42,4 +42,22 @@ def test_draw_pixels():
         pico_output_draw_pixels(pixels)
         screenshot_and_compare("draw_pixel.png")
     finally:
+        pico_init(0)
+
+def test_draw_diagonal_pixels():
+    pico_init(1)
+    try:
+        original_dim_window = pico_get_dim_window()
+        original_dim_world = pico_get_dim_world()
+        pico_set_dim_window((160, 160))
+        pico_set_dim_world((16, 16))
+        pico_set_zoom((100, 100))
+        for i in range(16):
+            pico_output_draw_pixel((i, i))
+            pico_output_draw_pixel((15 - i, i))
+        pico_output_present()
+        screenshot_and_compare("diagonal_pixels.png")
+    finally:
+        pico_set_dim_window(original_dim_window)
+        pico_set_dim_world(original_dim_world)
         pico_init(0)
